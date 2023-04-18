6b182ec45de98dba0d66270d7b2497a2f5713047
eriskjel <eriskjel@stud.ntnu.no>
2023-04-18 11:56:01 +0200

    created UserRepository

---
+++ backend/src/main/java/ntnu/idatt2106/backend/repository/UserRepository.java
@@ -0,0 +1,13 @@
+package ntnu.idatt2106.backend.repository;
+
+import ntnu.idatt2106.backend.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByEmail(String email);
+
+}

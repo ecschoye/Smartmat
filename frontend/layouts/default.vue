a7afe0ce3a5df392eada9829438b577ca532f7b9
eriskjel <eriskjel@stud.ntnu.no>
2023-04-19 15:27:30 +0200

    added green theme to global slot

--- frontend/layouts/default.vue
+++ frontend/layouts/default.vue
@@ -1,7 +1,18 @@
 <template>
   <div>
     <NavBar/>
-    <slot />
+    <div class="page-container">
+      <slot />
+    </div>
+
   </div>
 </template>
+
+<style scoped>
+.page-container {
+  background-color: #4ade80;
+  min-height: calc(100vh - 96px);
+  display: block;
+}
+</style>
 

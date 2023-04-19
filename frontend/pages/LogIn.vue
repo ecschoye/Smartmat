c67168b27b3f4870c6c0c25924f7fe6a7aa733d0
eriskjel <eriskjel@stud.ntnu.no>
2023-04-19 15:26:41 +0200

    created login page

---
+++ frontend/pages/LogIn.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <LogInForm id="log-in-form"> </LogInForm>
+  </div>
+</template>
+
+<script setup>
+
+import LogInForm from "~/components/Form/LogInForm.vue";
+</script>
+
+<style scoped>
+#log-in-form{
+  display: flex;
+  justify-content: center;
+
+}
+</style>

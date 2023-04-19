c0ab1d845b633950f6520873021b603a91a1140e
eriskjel <eriskjel@stud.ntnu.no>
2023-04-19 15:27:39 +0200

    created base input component

---
+++ frontend/components/Form/BaseInput.vue
@@ -0,0 +1,114 @@
+<template>
+  <div class="input-container">
+    <input
+        :id="id"
+        placeholder=" "
+        class="input"
+        :type="type"
+        :value="modelValue"
+        required
+        @input="$emit('update:modelValue', $event.target.value)"
+    >
+    <div class="cut"></div>
+    <label :for="id" class="placeholder">{{ label }}</label>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  props: {
+    id: {
+      type: String,
+      required: true,
+    },
+    label: {
+      type: String,
+      default: '',
+    },
+    modelValue: {
+      type: [String, Number],
+      default: '',
+    },
+    type: {
+      type: String,
+      default: 'text',
+    },
+  },
+});
+</script>
+
+
+
+
+
+
+
+<style scoped>
+.input-container {
+  height: 50px;
+  position: relative;
+  width: 100%;
+  margin-bottom: 20px;
+}
+
+.input{
+  background-color: rgb(92, 88, 88);
+  border-radius: 12px;
+  border: 0;
+  box-sizing: border-box;
+  color: #eee;
+  font-size: 18px;
+  height: 100%;
+  outline: 0;
+  padding: 4px 20px 0;
+  width: 100%;
+}
+.cut{
+  background-color: rgba(92, 88, 88,0);
+  border-radius: 10px;
+  height: 20px;
+  left: 20px;
+  position: absolute;
+  top: -20px;
+  transform: translateY(0);
+  transition: transform 200ms;
+  width: 70px;
+}
+
+.input:focus ~ .cut,
+.input:not(:placeholder-shown) ~ .cut {
+  transform: translateY(8px);
+  background-color: rgba(92, 88, 88,1);
+}
+
+.placeholder {
+  color: white;
+  font-family: sans-serif;
+  left: 20px;
+  line-height: 14px;
+  pointer-events: none;
+  position: absolute;
+  transform-origin: 0 50%;
+  transition: transform 200ms, color 200ms;
+  top: 20px;
+}
+
+::placeholder {
+  color: white;
+}
+
+.input:focus ~ .placeholder,
+.input:not(:placeholder-shown) ~ .placeholder {
+  transform: translateY(-30px) translateX(10px) scale(0.75);
+}
+
+.input:not(:placeholder-shown) ~ .placeholder {
+  color: #808097;
+}
+
+.input:focus ~ .placeholder {
+  color: white;
+}
+</style>

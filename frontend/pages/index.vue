2df8e41d4b385458aedc18ff024d100ecd7178b8
eriskjel <eriskjel@stud.ntnu.no>
2023-04-19 15:26:57 +0200

    created login page

--- frontend/pages/index.vue
+++ frontend/pages/index.vue
@@ -1,5 +1,10 @@
 <template>
-  <div>
-    <NuxtWelcome/>
+  <div class="h-screen flex items-center justify-center">
+    <NuxtLink to="/login">
+      <button class="bg-white text-green-700 hover:bg-green-700 hover:text-white font-bold py-2 px-4 rounded">
+        Go to log in
+      </button>
+    </NuxtLink>
   </div>
 </template>
+

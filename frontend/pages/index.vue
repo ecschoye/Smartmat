944d76a8ada90f9a5f4910630844415f9684f7c2
eriskjel <eriskjel@stud.ntnu.no>
2023-04-19 15:56:22 +0200

    added small design change to home page

--- frontend/pages/index.vue
+++ frontend/pages/index.vue
@@ -1,7 +1,12 @@
 <template>
-  <div class="h-screen flex items-center justify-center">
+  <div class="flex justify-center">
+    <h1 class="text-white text-6xl mb-20 mt-20">Velkommen til SmartMat</h1>
+  </div>
+
+  <div class="h-screen flex justify-center">
+
     <NuxtLink to="/login">
-      <button class="bg-white text-green-700 hover:bg-green-700 hover:text-white font-bold py-2 px-4 rounded">
+      <button class="bg-white text-green-700 hover:bg-green-700 hover:text-white font-bold py-2 px-4 rounded transform hover:scale-100">
         Go to log in
       </button>
     </NuxtLink>

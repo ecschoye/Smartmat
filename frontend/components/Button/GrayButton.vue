c797627ce0c0a65ad045af28d06b1fd100bc32d9
eriskjel <eriskjel@stud.ntnu.no>
2023-04-19 23:27:05 +0200

    created generic button componts

---
+++ frontend/components/Button/GrayButton.vue
@@ -0,0 +1,21 @@
+<template>
+  <button class="text-black bg-gray-400 rounded hover:bg-gray-600" :style="{ width: width, height: height }">
+    {{ label }}
+  </button>
+</template>
+
+<script setup>
+const { label, width = '', height = '' } = defineProps({
+  label: String,
+  width: String,
+  height: String
+});
+</script>
+
+<style scoped>
+button {
+  border: none;
+  outline: none;
+  cursor: pointer;
+}
+</style>
---
+++ frontend/components/Button/GreenButton.vue
@@ -0,0 +1,23 @@
+<template>
+  <button class="text-white rounded hover:bg-green-700" :style="{ width: width, height: height }">
+    {{ label }}
+  </button>
+</template>
+
+<script setup>
+const { label, width = '', height = '' } = defineProps({
+  label: String,
+  width: String,
+  height: String
+});
+</script>
+
+<style scoped>
+button {
+  background-color: #31C48D;
+  border: none;
+  outline: none;
+  cursor: pointer;
+  transition: all 0.2s;
+}
+</style>

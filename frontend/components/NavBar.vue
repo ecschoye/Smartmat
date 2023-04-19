65824bfb7d37f6acd9186406f78e7ded12777c02
Kristian Vaula <kristvje@stud.ntnu.no>
2023-04-19 15:51:03 +0200

    Added FridgeSelector and settings button to navbar

--- frontend/components/NavBar.vue
+++ frontend/components/NavBar.vue
@@ -2,23 +2,35 @@
 <header class="bg-white">
   <nav class="mx-auto flex max-w-7xl items-center p-6 lg:px-8" aria-label="Global">
     <div class="hidden lg:flex flex-1 space-x-5 items-center justify-start">
-      <a href="/" class="-m-1.5 p-1.5">
+      <NuxtLink href="/" class="-m-1.5 p-1.5">
         <span class="sr-only">Your Company</span>
-        <img class="h-8 w-auto" src="../assets/icons/smartmat/leafTransparent.png" alt="">
-      </a>
-      <a href="#" class="text-sm font-semibold leading-6 text-gray-900">Hjem</a>
-      <a href="#" class="text-sm font-semibold leading-6 text-gray-900">Ukesmeny</a>
-      <a href="#" class="text-sm font-semibold leading-6 text-gray-900">Oppskrifter</a>
-      <a href="#" class="text-sm font-semibold leading-6 text-gray-900">Statistikk</a>
+        <img class="h-12 w-auto" src="../assets/icons/smartmat/leafTransparent.png" alt="">
+      </NuxtLink>
+      <NuxtLink href="#" class="text-md font-semibold leading-6 text-gray-900">Hjem</NuxtLink>
+      <NuxtLink href="#" class="text-md font-semibold leading-6 text-gray-900">Ukesmeny</NuxtLink>
+      <NuxtLink href="#" class="text-md font-semibold leading-6 text-gray-900">Oppskrifter</NuxtLink>
+      <NuxtLink href="#" class="text-md font-semibold leading-6 text-gray-900">Statistikk</NuxtLink>
     </div>
-    <div class="hidden lg:flex lg:flex-1 space-x-2 items-center lg:justify-end">
-      <a href="/" title="Varslinger" class="-m-1.5 p-1.5 text-sm font-semibold leading-6 text-gray-900">
+      <div class="hidden lg:flex flex-1 space-x-2 items-center lg:justify-end">
+        <div class="p-0.5 rounded-md ring-1 ring-inset ring-gray-300 inline-flex items-center">
+          <FridgeSelector :fridges="fridges" @selected-fridge-event="handleFridgeEvent" />
+          <button type="button" 
+            class="inline-flex items-center hover:cursor-pointer
+               bg-white sm:text-md outline:none
+              transition duration-150 hover:border-emerald-400
+              p-2 pressed:border-emerald-600 border-l border-gray-300">
+            <img class="h-5 w-auto mr-2 pt-0.5" src="../assets/icons/settings.png" alt="">
+            Administrer
+          </button>
+        </div>
+      <div class="w-8"></div>
+      <NuxtLink href="/" title="Varslinger" class="-m-1.5 p-1.5 text-sm font-semibold leading-6 text-gray-900">
         <span class="sr-only">Varslinger</span>
         <img class="h-8 w-auto" src="../assets/icons/bell.png" alt="">
-      </a>
+      </NuxtLink>
       <HeadlessMenu v-if="loggedInStatus" as="div" class="relative inline-block text-left">
         <div>
-          <HeadlessMenuButton class="inline-flex w-full justify-center gap-x-1.5 rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900" >
+          <HeadlessMenuButton title="Kontovalg og innstillinger" class="inline-flex w-full justify-center gap-x-1.5 rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900" >
             <img class="h-8 w-auto" src="../assets/icons/profile.png" alt="">
           </HeadlessMenuButton>
         </div>
@@ -27,10 +39,10 @@
           <HeadlessMenuItems class="absolute right-0 z-10 mt-2 w-56 origin-top-right rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
             <div class="py-1">
               <HeadlessMenuItem v-slot="{ active }">
-                <a href="#" :class="[active ? 'bg-gray-100 text-gray-900' : 'text-gray-700', 'block px-4 py-2 text-sm']">Rediger Profil</a>
+                <NuxtLink href="#" :class="[active ? 'bg-gray-100 text-gray-900' : 'text-gray-700', 'block px-4 py-2 text-sm']">Rediger Profil</NuxtLink>
               </HeadlessMenuItem>
               <HeadlessMenuItem v-slot="{ active }">
-                <a href="#" :class="[active ? 'bg-gray-100 text-gray-900' : 'text-gray-700', 'block px-4 py-2 text-sm']">Systeminnstillinger</a>
+                <NuxtLink href="#" :class="[active ? 'bg-gray-100 text-gray-900' : 'text-gray-700', 'block px-4 py-2 text-sm']">Systeminnstillinger</NuxtLink>
               </HeadlessMenuItem>
               <form method="POST" action="#">
                 <HeadlessMenuItem v-slot="{ active }">
@@ -41,10 +53,10 @@
           </HeadlessMenuItems>
         </transition>
       </HeadlessMenu>
-      <a v-else title="Logg inn" href="/" class="-m-1.5 p-1.5 text-sm font-semibold leading-6 text-gray-900">
+      <NuxtLink v-else title="Logg inn" href="/" class="-m-1.5 p-1.5 text-sm font-semibold leading-6 text-gray-900">
         <span class="sr-only">Logg Inn</span>
         <img class="h-8 w-auto" src="../assets/icons/profile.png" alt="">
-      </a>
+      </NuxtLink>
     </div>
   </nav>
   </header>
@@ -54,7 +66,31 @@
 export default {
   data () {
     return {
+      fridges : [
+        {
+          id: 1,
+          name: 'Hjemme'
+        },
+        {
+          id: 2,
+          name: 'Hytta'
+        },
+        {
+          id: 3,
+          name: 'Kjartans Hus',
+        },
+        {
+          id: 4,
+          name: 'Kaspers Lagoon',
+        }
+      ],
+      selected: -1,
       loggedInStatus: true
+    }
+  },
+  methods : {
+    handleFridgeEvent(value) {
+      this.selected = value; 
     }
   }
 }

9e9abdc70654d937ae860bef17c797e46d5afd1f
Hans Magne Asheim <hmasheim@stud.ntnu.no>
2023-04-20 13:34:19 +0200

    edit: shopping list controller
    Edited the parameters in the endpoints

--- backend/src/main/java/ntnu/idatt2106/backend/controller/ShoppingListController.java
+++ backend/src/main/java/ntnu/idatt2106/backend/controller/ShoppingListController.java
@@ -5,6 +5,7 @@ import lombok.RequiredArgsConstructor;
 import ntnu.idatt2106.backend.exceptions.SaveException;
 import ntnu.idatt2106.backend.model.Grocery;
 import ntnu.idatt2106.backend.model.GroceryShoppingList;
+import ntnu.idatt2106.backend.model.requests.SaveGroceryRequest;
 import ntnu.idatt2106.backend.service.ShoppingListService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -25,10 +26,10 @@ public class ShoppingListController {
 
     Logger logger = LoggerFactory.getLogger(ShoppingListController.class);
 
-    @PostMapping("/create")
-    public ResponseEntity<Integer> createShoppingList(int refrigeratorId) throws SaveException {
+    @PostMapping("/create/{refrigeratorId}")
+    public ResponseEntity<Long> createShoppingList(@PathVariable(name = "refrigeratorId") long refrigeratorId) throws SaveException {
         logger.info("Received request to create shopping list for refrigerator with id {}", refrigeratorId);
-        int shoppingListId = shoppingListService.createShoppingList(refrigeratorId);
+        long shoppingListId = shoppingListService.createShoppingList(refrigeratorId);
         if (shoppingListId == -1) {
             logger.info("Failed to create shopping list for refrigerator id {}", refrigeratorId);
             throw new SaveException("Failed to create shopping list for refrigerator id " + refrigeratorId);
@@ -38,7 +39,7 @@ public class ShoppingListController {
     }
 
     @GetMapping("/groceries/{shoppingListId}")
-    public ResponseEntity<List<Grocery>> getGroceriesFromShoppingList(@PathVariable int shoppingListId) throws NullPointerException {
+    public ResponseEntity<List<Grocery>> getGroceriesFromShoppingList(@PathVariable(name="shoppingListId") long shoppingListId) throws NullPointerException {
         logger.info("Received request to get groceries in shopping list with id {}", shoppingListId);
         List<Grocery> groceries = shoppingListService.getGroceries(shoppingListId);
         if (groceries.isEmpty()) {
@@ -49,10 +50,10 @@ public class ShoppingListController {
         return new ResponseEntity<>(groceries, HttpStatus.OK);
     }
 
-    @PostMapping("/add-grocery")
-    public ResponseEntity<GroceryShoppingList> saveGroceryToShoppingList(Grocery grocery, int shoppingListId, boolean isRequested) throws SaveException{
-        logger.info("Received request to save grocery {} to shopping list with id {}", grocery, shoppingListId);
-        Optional<GroceryShoppingList> groceryList = shoppingListService.saveGrocery(grocery, shoppingListId, isRequested);
+    @PostMapping("/add-grocery/{shoppingListId}/{isRequested}")
+    public ResponseEntity<GroceryShoppingList> saveGroceryToShoppingList(@RequestBody SaveGroceryRequest groceryRequest) throws SaveException{
+        logger.info("Received request to save grocery {} to shopping list with id {}", groceryRequest.getName(), groceryRequest.getShoppingListId());
+        Optional<GroceryShoppingList> groceryList = shoppingListService.saveGrocery(groceryRequest);
         if (groceryList.isEmpty()) {
             logger.info("No registered changes to grocery is saved");
             throw new SaveException("Failed to add a new grocery to shopping list");

9616887bde1c6e6d87453a100863250f42e059b7
Hans Magne Asheim <hmasheim@stud.ntnu.no>
2023-04-20 13:34:54 +0200

    init: started to write som test
    Started creating an own test database

---
+++ backend/src/test/java/ntnu/idatt2106/backend/config/TestDatabaseJpaConfig.java
@@ -0,0 +1,32 @@
+package ntnu.idatt2106.backend.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+import javax.sql.DataSource;
+
+@Configuration
+@PropertySource("application-test.properties")
+@EnableTransactionManagement
+public class TestDatabaseJpaConfig {
+
+    @Autowired
+    private Environment environment;
+
+    @Bean
+    public DataSource dataSource() {
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+
+        dataSource.setDriverClassName(environment.getProperty("spring.datasource.driverClassNam"));
+        dataSource.setUrl(environment.getProperty("spring.datasource.url"));
+        dataSource.setUsername(environment.getProperty("spring.datasource.username"));
+        dataSource.setPassword(environment.getProperty("spring.datasource.password"));
+
+        return dataSource;
+    }
+}

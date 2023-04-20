474f38fb720cab8389bbdef6a87f832fe40e832a
Kristian Vaula <kristvje@stud.ntnu.no>
2023-04-20 16:15:03 +0200

    Minor

--- backend/src/main/java/ntnu/idatt2106/backend/repository/RefrigeratorUserRepository.java
+++ backend/src/main/java/ntnu/idatt2106/backend/repository/RefrigeratorUserRepository.java
@@ -3,5 +3,7 @@ package ntnu.idatt2106.backend.repository;
 import ntnu.idatt2106.backend.model.RefrigeratorUser;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface RefrigeratorUserRepository extends JpaRepository<RefrigeratorUser, Long> {
 }

97ad1ccc4f3857760963adc11a9f212893129959
daniesky <daniesky@stud.ntnu.no>
2023-04-21 11:30:30 +0200

    Frontend type and store mods

--- frontend/store/refridgeratorStore.ts
+++ frontend/store/refridgeratorStore.ts
@@ -1,14 +1,116 @@
 import { defineStore } from 'pinia'
 import { Category } from '~/types/CategoryType';
-import type { Grocery } from '~/types/GroceryType'
+import type { GroceryEntity } from '~/types/GroceryEntityType'
+import { Grocery } from '~/types/GroceryType';
 
 export const useRefridgeratorStore = defineStore('refridgerator', {
     state: () => ({
-        groceries : [] as Grocery[],
-        categories : [] as Category[],
+        groceries : [
+            {
+               id:1,
+               physicalExpiryDate:'20-04-2023',
+               fridgeId: 1,
+               grocery: {
+                id: 1,
+                name: "Banan",
+                description: 'Gul banan',
+                groceryExpiryDate: 14,
+                subCategory: {
+                    id:1,
+                    name: 'Gul frukt',
+                    category:{
+                        id:1,
+                        name: 'Frukt'
+                    }
+                   },
+               }
+            },
+            {
+                id:2,
+                physicalExpiryDate:'20-04-2023',
+                fridgeId: 1,
+                grocery: {
+                 id: 1,
+                 name: "Banan",
+                 description: 'Gul banan',
+                 groceryExpiryDate: 14,
+                 subCategory: {
+                     id:1,
+                     name: 'Gul frukt',
+                     category:{
+                         id:1,
+                         name: 'Frukt'
+                     }
+                    },
+                }
+             },
+             {
+                id:3,
+                physicalExpiryDate:'20-04-2023',
+                fridgeId: 1,
+                grocery: {
+                 id: 1,
+                 name: "Banan",
+                 description: 'Gul banan',
+                 groceryExpiryDate: 14,
+                 subCategory: {
+                     id:1,
+                     name: 'Gul frukt',
+                     category:{
+                         id:1,
+                         name: 'Frukt'
+                     }
+                    },
+                }
+             },
+             {
+                id:4,
+                physicalExpiryDate:'20-04-2023',
+                fridgeId: 1,
+                grocery: {
+                 id: 1,
+                 name: "Banan",
+                 description: 'Gul banan',
+                 groceryExpiryDate: 14,
+                 subCategory: {
+                     id:1,
+                     name: 'Gul frukt',
+                     category:{
+                         id:1,
+                         name: 'Frukt'
+                     }
+                    },
+                }
+             },
+             {
+                id:5,
+                physicalExpiryDate:'25-04-2023',
+                fridgeId: 1,
+                grocery: {
+                 id: 2,
+                 name: "Melk",
+                 description: 'Melk 2% fett',
+                 groceryExpiryDate: 4,
+                 subCategory: {
+                     id:2,
+                     name: 'Melk',
+                     category:{
+                         id:2,
+                         name: 'Melkeprodukter'
+                     }
+                    },
+                }
+             },
+
+       ] as GroceryEntity[],
+        categories : [
+            {
+
+            }
+        ] as Category[],
     }),
     getters: {
-        getGroceries: (state) : Grocery[] => {
+        getGroceries: (state) : GroceryEntity[] => {
             return state.groceries;
         },
         getCategories: (state) : Category[] => {

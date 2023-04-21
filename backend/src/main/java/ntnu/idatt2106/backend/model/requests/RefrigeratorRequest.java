114665dad16d51232f27eaa92506df3d4814dfac
Kristian Vaula <kristvje@stud.ntnu.no>
2023-04-21 11:02:30 +0200

    RefrigeratorRequest set to NotNull

--- backend/src/main/java/ntnu/idatt2106/backend/model/requests/RefrigeratorRequest.java
+++ backend/src/main/java/ntnu/idatt2106/backend/model/requests/RefrigeratorRequest.java
@@ -1,9 +1,13 @@
 package ntnu.idatt2106.backend.model.requests;
 
+import jakarta.validation.constraints.NotNull;
 import ntnu.idatt2106.backend.model.Refrigerator;
 
 public class RefrigeratorRequest {
+
+    @NotNull
     private String username;
+    @NotNull
     private Refrigerator refrigerator;
 
     public String getUsername() {

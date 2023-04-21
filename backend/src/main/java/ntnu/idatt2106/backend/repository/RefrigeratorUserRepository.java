475e06cd0501569ac0505da04b03da21f6091163
Kristian Vaula <kristvje@stud.ntnu.no>
2023-04-21 14:42:58 +0200

    Added find method to RefrigeratorUserRepository

--- backend/src/main/java/ntnu/idatt2106/backend/repository/RefrigeratorUserRepository.java
+++ backend/src/main/java/ntnu/idatt2106/backend/repository/RefrigeratorUserRepository.java
@@ -1,10 +1,14 @@
 package ntnu.idatt2106.backend.repository;
 
+import ntnu.idatt2106.backend.model.Refrigerator;
 import ntnu.idatt2106.backend.model.RefrigeratorUser;
+import ntnu.idatt2106.backend.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 
 public interface RefrigeratorUserRepository extends JpaRepository<RefrigeratorUser, Long> {
     Optional<RefrigeratorUser> findByUser_IdAndRefrigerator_Id(String userId, long refrigeratorId);
+
+    Optional<RefrigeratorUser> findByUserAndRefrigerator(User user, Refrigerator refrigerator);
 }

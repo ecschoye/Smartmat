c9b678cd3d3f54d53b938d9195cf94c211708b7f
ecschoye <ecschoye@stud.ntnu.no>
2023-04-23 16:15:44 +0200

    update config

--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -5,6 +5,7 @@ export default defineNuxtConfig({
     'nuxt-headlessui',
     '@pinia/nuxt',
     '@nuxtjs/color-mode',
+    '@nuxtjs/i18n',
   ],
   colorMode: {
     classSuffix: ''
@@ -16,5 +17,60 @@ export default defineNuxtConfig({
     config: {},
     injectPosition: 0,
     viewer: true,
-  }
+  },
+  i18n: {
+    lazy: true,
+    langDir: 'locales',
+    strategy: 'prefix',
+    locales: [
+      {
+        code: 'no',
+        iso: 'no',
+        name: 'Norsk',
+        file: 'no.json',
+      },
+      {
+        code: 'en',
+        iso: 'en',
+        name: 'English (US)',
+        file: 'en.json',
+      },
+      {
+        code: 'de',
+        iso: 'de',
+        name: 'Deutsch',
+        file: 'de.json',
+      },
+      {
+        code: 'fr',
+        iso: 'fr',
+        name: 'Français',
+        file: 'fr.json',
+      },
+      {
+        code: 'es',
+        iso: 'es',
+        name: 'Español',
+        file: 'es.json',
+      },
+      {
+        code: 'zh',
+        iso: 'zh',
+        name: '中文 ',
+        file: 'zh.json',
+      },
+      {
+        code: 'hi',
+        iso: 'hi',
+        name: 'हिंदी (Hindi) ',
+        file: 'hi.json',
+      },
+    ],
+    customRoutes: 'config',
+    pages: {
+    },
+    defaultLocale: 'no',
+    vueI18n: '/i18n.config.ts'
+  },
+
 })

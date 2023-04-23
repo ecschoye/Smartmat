00984bb70138d9ea822f4a7badc72beaa4edf8cf
ecschoye <ecschoye@stud.ntnu.no>
2023-04-23 16:16:46 +0200

    added @nuxtjs/i18n to package.json

--- frontend/pages/account-details.vue
+++ frontend/pages/account-details.vue
@@ -1,19 +1,19 @@
 <template>
   <div class="flex items-center justify-center">
     <div class="w-2/3 mx-auto bg-white dark:bg-zinc-500 mt-10 rounded-md py-5">
-      <p class="text-center dark:text-white mt-10 text-xl font-bold">Kontoinnstillinger</p>
+      <p class="text-center dark:text-white mt-10 text-xl font-bold">{{t('account_settings')}}</p>
       <div class="w-2/3 mx-auto flex flex-col items-center mt-3">
         <div class="divider"></div>
         <div class="mb-4">
-          <BaseInput id="name" type="name" class="input-container" label="Navn"/>
-          <BaseInput id="email" type="email" class="input-container" label="E-post"/>
+          <BaseInput id="name" type="name" class="input-container" :label="$t('name')"/>
+          <BaseInput id="email" type="email" class="input-container" :label="$t('email')"/>
         </div>
         <div class="flex mb-4">
-          <GrayButton @click="submit" label="Oppdater" width="100%" height="50px" class="mr-5 px-3"/>
-          <GrayButton label="Gå tilbake" width="100%" height="50px" class="px-3 text-center"/>
+          <GrayButton @click="submit" :label="$t('update')" width="100%" height="50px" class="mr-5 px-3"/>
+          <GrayButton :label="$t('go_back')" width="100%" height="50px" class="px-3 text-center"/>
         </div>
         <NuxtLink to="/change-password" class="w-30 rounded bg-gray-200 text-gray-800 text-center py-2 px-4">
-          Endre passord
+          {{t('change_password')}}
         </NuxtLink>
       </div>
     </div>

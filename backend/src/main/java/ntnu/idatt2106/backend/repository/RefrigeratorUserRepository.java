284676540e10c14ae10d44b71b551bb7b2bf4dce
Kristian Vaula <kristvje@stud.ntnu.no>
2023-04-24 08:16:11 +0200

    RefrigeratorUserRepository findByRefrigeratorIdAndRole method

--- backend/src/main/java/ntnu/idatt2106/backend/repository/RefrigeratorUserRepository.java
+++ backend/src/main/java/ntnu/idatt2106/backend/repository/RefrigeratorUserRepository.java
@@ -3,12 +3,16 @@ package ntnu.idatt2106.backend.repository;
 import ntnu.idatt2106.backend.model.Refrigerator;
 import ntnu.idatt2106.backend.model.RefrigeratorUser;
 import ntnu.idatt2106.backend.model.User;
+import ntnu.idatt2106.backend.model.enums.Role;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface RefrigeratorUserRepository extends JpaRepository<RefrigeratorUser, Long> {
-    Optional<RefrigeratorUser> findByUser_IdAndRefrigerator_Id(String userId, long refrigeratorId);
+    Optional<RefrigeratorUser> findByIdAndRefrigerator_Id(String userId, long refrigeratorId);
 
     Optional<RefrigeratorUser> findByUserAndRefrigerator(User user, Refrigerator refrigerator);
+
+    List<RefrigeratorUser> findByRefrigeratorIdAndRole(long refrigeratorId, Role role);
 }

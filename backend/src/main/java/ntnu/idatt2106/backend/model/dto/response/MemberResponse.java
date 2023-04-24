49a50653cfd828e07d128ff9ecf588cc43c6b6dd
Kristian Vaula <kristvje@stud.ntnu.no>
2023-04-24 13:13:44 +0200

    MemberResponse now includes name

--- backend/src/main/java/ntnu/idatt2106/backend/model/dto/response/MemberResponse.java
+++ backend/src/main/java/ntnu/idatt2106/backend/model/dto/response/MemberResponse.java
@@ -5,6 +5,7 @@ import ntnu.idatt2106.backend.model.enums.Role;
 
 public class MemberResponse {
     private long refrigeratorId;
+    private String name;
     private String username;
     private Role role;
 
@@ -12,8 +13,17 @@ public class MemberResponse {
 
     public MemberResponse(RefrigeratorUser ru) {
         this.refrigeratorId = ru.getRefrigerator().getId();
+        this.name = ru.getUser().getName();
         this.username = ru.getUser().getUsername();
         this.role = ru.getRole();
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
     }
 
     public long getRefrigeratorId() {

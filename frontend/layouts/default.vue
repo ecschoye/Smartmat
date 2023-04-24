7c7967bea6e6e14a28c94b559e6d36cb4a5e2f4c
ecschoye <ecschoye@stud.ntnu.no>
2023-04-24 11:20:24 +0200

    fix default background color in light/ dark mode

--- frontend/layouts/default.vue
+++ frontend/layouts/default.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="wrapper">
     <NavBar/>
-    <div class="page-container">
+    <div class="page-container light-background-color dark:dark-background-color">
       <slot />
     </div>
 
@@ -10,7 +10,6 @@
 
 <style scoped>
 .page-container {
-  background-color: #31C48D;
   min-height: calc(100vh - 96px);
   display: block;
   max-height: 100vh;

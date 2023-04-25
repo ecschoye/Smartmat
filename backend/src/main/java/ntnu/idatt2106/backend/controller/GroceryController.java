77e43faa0b5945e87c6cb5518df164f399ca087d
Kristian Vaula <kristvje@stud.ntnu.no>
2023-04-25 16:41:34 +0200

    Groceries by refrigerator endpoint added to GroceryController

--- backend/src/main/java/ntnu/idatt2106/backend/controller/GroceryController.java
+++ backend/src/main/java/ntnu/idatt2106/backend/controller/GroceryController.java
@@ -1,5 +1,56 @@
 package ntnu.idatt2106.backend.controller;
 
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import ntnu.idatt2106.backend.exceptions.RefrigeratorNotFoundException;
+import ntnu.idatt2106.backend.exceptions.UnauthorizedException;
+import ntnu.idatt2106.backend.exceptions.UserNotFoundException;
+import ntnu.idatt2106.backend.model.dto.RefrigeratorGroceryDTO;
+import ntnu.idatt2106.backend.service.GroceryService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
+/**
+ * Controller regarding the groceries inside a refrigerator
+ */
+@RestController
+@RequestMapping("/api/refrigerator/grocery")
+@RequiredArgsConstructor
 public class GroceryController {
+
+    private final GroceryService groceryService;
+    Logger logger = LoggerFactory.getLogger(GroceryController.class);
+
+    @GetMapping("/{refrigeratorId}")
+    public ResponseEntity<List<RefrigeratorGroceryDTO>> getGroceriesByRefrigerator(@Valid @PathVariable long refrigeratorId, HttpServletRequest httpServletRequest) throws UserNotFoundException, UnauthorizedException, RefrigeratorNotFoundException {
+        logger.info("Received request for groceries by refrigerator with id: {}", refrigeratorId);
+        return new ResponseEntity<>(groceryService.getGroceriesByRefrigerator(refrigeratorId, httpServletRequest), HttpStatus.OK);
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
--- backend/src/test/java/ntnu/idatt2106/backend/controller/GroceryControllerTest.java
+++ backend/src/test/java/ntnu/idatt2106/backend/controller/GroceryControllerTest.java
@@ -1,4 +1,67 @@
 package ntnu.idatt2106.backend.controller;
 
-public class GroceryControllerTest {
+import ntnu.idatt2106.backend.exceptions.RefrigeratorNotFoundException;
+import ntnu.idatt2106.backend.exceptions.UnauthorizedException;
+import ntnu.idatt2106.backend.exceptions.UserNotFoundException;
+import ntnu.idatt2106.backend.model.dto.RefrigeratorGroceryDTO;
+import ntnu.idatt2106.backend.service.GroceryService;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.mock.web.MockHttpServletRequest;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.mockito.Mockito.when;
+
+class GroceryControllerTest {
+
+    @Mock
+    private GroceryService groceryService;
+
+    @InjectMocks
+    private GroceryController groceryController;
+
+    private MockHttpServletRequest httpServletRequest;
+
+    @BeforeEach
+    void setUp() {
+        MockitoAnnotations.openMocks(this);
+        httpServletRequest = new MockHttpServletRequest();
+    }
+
+    @Test
+    void getGroceriesByRefrigerator_validInput_returnsGroceries() throws UserNotFoundException, UnauthorizedException, RefrigeratorNotFoundException {
+        // Arrange
+        long refrigeratorId = 1L;
+        RefrigeratorGroceryDTO groceryDTO = new RefrigeratorGroceryDTO();
+        List<RefrigeratorGroceryDTO> expectedList = new ArrayList<>();
+        expectedList.add(groceryDTO);
+        when(groceryService.getGroceriesByRefrigerator(refrigeratorId, httpServletRequest)).thenReturn(expectedList);
+
+        // Act
+        ResponseEntity<List<RefrigeratorGroceryDTO>> responseEntity = groceryController.getGroceriesByRefrigerator(refrigeratorId, httpServletRequest);
+
+        // Assert
+        Assertions.assertEquals(HttpStatus.OK, responseEntity.getStatusCode());
+        Assertions.assertEquals(expectedList, responseEntity.getBody());
+    }
+
+    @Test
+    void getGroceriesByRefrigerator_invalidRefrigeratorId_throwsRefrigeratorNotFoundException() throws UserNotFoundException, UnauthorizedException, RefrigeratorNotFoundException {
+        // Arrange
+        long refrigeratorId = 1L;
+        when(groceryService.getGroceriesByRefrigerator(refrigeratorId, httpServletRequest)).thenThrow(new RefrigeratorNotFoundException("Refrigerator not found"));
+
+        // Act and Assert
+        Assertions.assertThrows(RefrigeratorNotFoundException.class, () -> {
+            groceryController.getGroceriesByRefrigerator(refrigeratorId, httpServletRequest);
+        });
+    }
 }

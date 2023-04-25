4fc3f07bf426795720c2e02664a4ae1e99637995
Kristian Vaula <kristvje@stud.ntnu.no>
2023-04-25 16:22:39 +0200

    findByRefrigerator method added in RefrigeratorGroceryRepository

--- backend/src/main/java/ntnu/idatt2106/backend/repository/RefrigeratorGroceryRepository.java
+++ backend/src/main/java/ntnu/idatt2106/backend/repository/RefrigeratorGroceryRepository.java
@@ -1,7 +1,12 @@
 package ntnu.idatt2106.backend.repository;
 
+import ntnu.idatt2106.backend.model.Refrigerator;
 import ntnu.idatt2106.backend.model.RefrigeratorGrocery;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface RefrigeratorGroceryRepository extends JpaRepository<RefrigeratorGrocery,Long> {
+
+    List<RefrigeratorGrocery> findByRefrigerator(Refrigerator refrigerator);
 }

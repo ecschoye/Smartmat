3a5899135374804aa567ec5f0cb47b409b67b860
daniesky <daniesky@stud.ntnu.no>
2023-04-25 13:23:30 +0200

    Created controller for notifications

---
+++ backend/src/main/java/ntnu/idatt2106/backend/controller/NotificationController.java
@@ -0,0 +1,63 @@
+package ntnu.idatt2106.backend.controller;
+
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.RequiredArgsConstructor;
+import ntnu.idatt2106.backend.model.User;
+import ntnu.idatt2106.backend.model.dto.GroceryNotificationDTO;
+import ntnu.idatt2106.backend.model.dto.response.ErrorResponse;
+import ntnu.idatt2106.backend.service.JwtService;
+import ntnu.idatt2106.backend.service.NotificationService;
+import ntnu.idatt2106.backend.service.SessionStorageService;
+import ntnu.idatt2106.backend.service.UserService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.logging.Logger;
+
+@RestController
+@RequestMapping("api/notifications")
+@RequiredArgsConstructor
+@Tag(name = "Notification controller", description = "Controller used to handle notifications sent to the user")
+
+public class NotificationController {
+
+    private final NotificationService notificationService;
+    private final UserService userService;
+    private final JwtService jwtService;
+    private final SessionStorageService sessionStorageService;
+
+    Logger logger = Logger.getLogger(NotificationController.class.getName());
+
+
+    @Operation(summary = "Retrieves all of the users notifications",
+            description = "Retrieves a users notifications",
+            responses = {
+            @ApiResponse(responseCode = "200", description = "If notifications were retrieved successfully",
+            content = @Content(mediaType = "application/json"))
+            }
+    )
+    @GetMapping("/all")
+    @PreAuthorize("IsAuthenticated()")
+    public ResponseEntity<?> getAll(HttpServletRequest request) {
+        User user = userService.findByEmail(jwtService.extractUsername(sessionStorageService.extractTokenFromAuthorizationHeader(request)));
+        logger.info("Received request for retrieving a users notifications");
+        try{
+            List<GroceryNotificationDTO> notifications = notificationService.getNotifications(user);
+            return ResponseEntity.ok(notifications);
+        }catch(Exception e){
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(new ErrorResponse("Error occured while retrieving notifications" + e.getMessage()));
+        }
+    }
+}

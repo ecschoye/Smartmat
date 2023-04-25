9da193b3f0b73343c83c1a04787010c6b5e98305
Kristian Vaula <kristvje@stud.ntnu.no>
2023-04-25 14:04:43 +0200

    RefrigeratorGroceryRepository

---
+++ backend/src/main/java/ntnu/idatt2106/backend/repository/RefrigeratorGroceryRepository.java
@@ -0,0 +1,7 @@
+package ntnu.idatt2106.backend.repository;
+
+import ntnu.idatt2106.backend.model.RefrigeratorGrocery;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RefrigeratorGroceryRepository extends JpaRepository<RefrigeratorGrocery,Long> {
+}

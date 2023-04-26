f3a09981faebe4f5b262f19fa684c78e093b1f3e
daniesky <daniesky@stud.ntnu.no>
2023-04-26 21:12:04 +0200

    partial merge

--- frontend/pages/home.vue
+++ frontend/pages/home.vue
@@ -1,12 +1,17 @@
 <template>
-    <div class="m-1">
-       <ShoppingList 
+    <div class="m-1 flex flex-col">
+        <div class="self-start">
+            <RefridgeratorWrapper /> 
+        </div>
+        <div class="self-end\">
+            <ShoppingList 
             :shopping-list = "shoppingList"
             :category-list = "categoryList"
             :suggestions-list = "suggestionsList"
             :shopping-cart = "shoppingCart"
             :refrigeratorId = "refrigeratorId"
         /> 
+        </div>
     </div>
 </template>
 

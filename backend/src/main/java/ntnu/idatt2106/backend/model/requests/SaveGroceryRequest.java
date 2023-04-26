ee1c5742e8c5b263925ca1f0e6050336468d7715
Kristian Vaula <kristvje@stud.ntnu.no>
2023-04-26 09:59:23 +0200

    Generalized saveGroceryRequest

--- backend/src/main/java/ntnu/idatt2106/backend/model/requests/SaveGroceryRequest.java
+++ backend/src/main/java/ntnu/idatt2106/backend/model/requests/SaveGroceryRequest.java
@@ -1,70 +1,23 @@
 package ntnu.idatt2106.backend.model.requests;
 
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import ntnu.idatt2106.backend.model.dto.GroceryDTO;
+
+/**
+ * Request to save a grocery
+ */
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class SaveGroceryRequest {
-    private String name;
-    private int groceryExpiryDays;
-    private String description;
-    private long subCategoryId;
-    private long foreignKey; //can be used for both shoppingListId and shoppingCartId
-    private int quantity;
 
-    public SaveGroceryRequest() {
-    }
-
-    public SaveGroceryRequest(String name, int groceryExpiryDays, String description, long subCategoryId, long foreignKey, int quantity) {
-        this.name = name;
-        this.groceryExpiryDays = groceryExpiryDays;
-        this.description = description;
-        this.subCategoryId = subCategoryId;
-        this.foreignKey = foreignKey;
-        this.quantity = quantity;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public int getGroceryExpiryDays() {
-        return groceryExpiryDays;
-    }
-
-    public void setGroceryExpiryDays(int groceryExpiryDays) {
-        this.groceryExpiryDays = groceryExpiryDays;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public long getSubCategoryId() {
-        return subCategoryId;
-    }
-
-    public void setSubCategoryId(long subCategoryId) {
-        this.subCategoryId = subCategoryId;
-    }
-
-    public long getForeignKey() {
-        return foreignKey;
-    }
-
-    public void setForeignKey(long foreignKey) {
-        this.foreignKey = foreignKey;
-    }
-
-    public int getQuantity() {
-        return quantity;
-    }
-
-    public void setQuantity(int quantity) {
-        this.quantity = quantity;
-    }
+    @NotNull
+    private long refrigeratorId;
+    @NotNull
+    private boolean custom;
+    @NotNull
+    private GroceryDTO grocery;
 }

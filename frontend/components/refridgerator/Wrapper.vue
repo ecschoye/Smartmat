308f521fb7e9894aeb7c113328f7711d4302871f
daniesky <daniesky@stud.ntnu.no>
2023-04-26 16:59:48 +0200

    minor error fix

--- frontend/components/refridgerator/Wrapper.vue
+++ frontend/components/refridgerator/Wrapper.vue
@@ -10,7 +10,7 @@
     </div>
 </template>
 
-<script setup lang="ts"> 
+<script setup lang="ts">
 import { useRefridgeratorStore } from '~/store/refridgeratorStore';
 import { getGroceriesByFridge } from '~/service/httputils/GroceryService';
 const refridgeratorStore = useRefridgeratorStore();

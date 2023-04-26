57271aaa4b37587409a0614d147a622364b87913
ecschoye <ecschoye@stud.ntnu.no>
2023-04-26 08:58:05 +0200

    changed back to strategy prefix in nuxt config

--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -21,7 +21,7 @@ export default defineNuxtConfig({
   i18n: {
     lazy: true,
     langDir: 'locales',
-    strategy: 'prefix_except_default',
+    strategy: 'prefix',
     locales: [
       {
         code: 'no',

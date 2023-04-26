0e01534d0a67222575bea27eccb75a0de0c8b5c5
eriskjel <eriskjel@stud.ntnu.no>
2023-04-26 12:55:34 +0200

    added lombok annotations to model classes

--- backend/src/main/java/ntnu/idatt2106/backend/model/requests/MemberRequest.java
+++ backend/src/main/java/ntnu/idatt2106/backend/model/requests/MemberRequest.java
@@ -1,12 +1,21 @@
 package ntnu.idatt2106.backend.model.requests;
 
 import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 import ntnu.idatt2106.backend.model.enums.FridgeRole;
 
 /**
  * Model used to receive a request for adding or editing a member
  * of a refrigerator.
  */
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class MemberRequest {
     @NotNull
     private String superName;
@@ -15,36 +24,4 @@ public class MemberRequest {
     @NotNull
     private String userName;
     private FridgeRole fridgeRole;
-
-    public String getSuperName() {
-        return superName;
-    }
-
-    public void setSuperName(String superName) {
-        this.superName = superName;
-    }
-
-    public long getRefrigeratorId() {
-        return refrigeratorId;
-    }
-
-    public void setRefrigeratorId(long refrigeratorId) {
-        this.refrigeratorId = refrigeratorId;
-    }
-
-    public String getUserName() {
-        return userName;
-    }
-
-    public void setUserName(String userName) {
-        this.userName = userName;
-    }
-
-    public FridgeRole getRole() {
-        return fridgeRole;
-    }
-
-    public void setRole(FridgeRole fridgeRole) {
-        this.fridgeRole = fridgeRole;
-    }
 }

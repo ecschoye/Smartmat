eb6e412a9d5d5ffd0d828a62d58d4c7916192b4a
Kristian Vaula <kristvje@stud.ntnu.no>
2023-04-28 09:32:47 +0200

    Added getRefrigeratorById API call

--- frontend/service/httputils/RefrigeratorService.ts
+++ frontend/service/httputils/RefrigeratorService.ts
@@ -1,10 +1,14 @@
 import axiosInstance from "~/service/AxiosInstance";
 import {AxiosResponse} from "axios";
 import {RefrigeratorRegisterData} from "~/types/RefrigeratorRegisterData";
-
+import type {Refrigerator} from "~/types/RefrigeratorType"; 
 
 export const getRefrigerators = async () : Promise<AxiosResponse> => {
     return axiosInstance.get('/api/refrigerator/user');
+}
+
+export const getRefrigeratorById = async(refrigeratorId : Number) : Promise<Refrigerator | null> => {
+    return axiosInstance.get(`/api/refrigerator/${refrigeratorId}`); 
 }
 
 export const postRegisterFridge = async (refrigeratorRegisterData : RefrigeratorRegisterData): Promise<AxiosResponse> => {

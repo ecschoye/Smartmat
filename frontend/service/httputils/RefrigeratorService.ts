1db737125057eaccca3c3127fcdb7939db3403c3
Kristian Vaula <kristvje@stud.ntnu.no>
2023-04-28 12:02:00 +0200

    Added more API calls to refrigerator

--- frontend/service/httputils/RefrigeratorService.ts
+++ frontend/service/httputils/RefrigeratorService.ts
@@ -2,15 +2,30 @@ import axiosInstance from "~/service/AxiosInstance";
 import {AxiosResponse} from "axios";
 import {RefrigeratorRegisterData} from "~/types/RefrigeratorRegisterData";
 import type {Refrigerator} from "~/types/RefrigeratorType"; 
+import type {Member} from "~/types/MemberType"
 
 export const getRefrigerators = async () : Promise<AxiosResponse> => {
     return axiosInstance.get('/api/refrigerator/user');
 }
 
-export const getRefrigeratorById = async(refrigeratorId : Number) : Promise<Refrigerator | null> => {
+export const getRefrigeratorById = async(refrigeratorId : Number) : Promise<AxiosResponse> => {
     return axiosInstance.get(`/api/refrigerator/${refrigeratorId}`); 
 }
 
 export const postRegisterFridge = async (refrigeratorRegisterData : RefrigeratorRegisterData): Promise<AxiosResponse> => {
     return axiosInstance.post('/api/refrigerator/new', refrigeratorRegisterData);
 }
+
+export const postAddMember = async (memberRequest : Member) : Promise<AxiosResponse> => {
+    return axiosInstance.post('/api/refrigerator/members/new', memberRequest); 
+}
+
+export const postEditMember = async (memberRequest : Member) : Promise<AxiosResponse> => {
+    return axiosInstance.post('/api/refrigerator/members/edit-role', memberRequest); 
+}
+
+export const postRemoveMember = async (memberRequest : Member) : Promise<AxiosResponse> => {
+    return axiosInstance.post('/api/refrigerator/members/remove', memberRequest); 
+}
+
+

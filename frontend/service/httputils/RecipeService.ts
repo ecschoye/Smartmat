4d52ce1348df068ab4e047e5102aca74e54db0ea
eriskjel <eriskjel@stud.ntnu.no>
2023-05-02 10:47:03 +0200

    migrated recipecatalog to use new backend logic

--- frontend/service/httputils/RecipeService.ts
+++ frontend/service/httputils/RecipeService.ts
@@ -1,7 +1,8 @@
 import axiosInstance from "../AxiosInstance";
 import type { AxiosResponse } from "axios";
 import axios from "axios";
+import {FetchRecipeDTO} from "~/types/FetchRecipeDTO";
 
-export const fetchRecipes = async (refrigeratorId : number) : Promise<AxiosResponse> => {
-    return axiosInstance.get(`/api/recipe/${refrigeratorId}`);
+export const fetchRecipes = async (fetchRecipeDTO: FetchRecipeDTO): Promise<AxiosResponse> => {
+    return axiosInstance.get('/api/recipe/fetch', { params: fetchRecipeDTO });
 }

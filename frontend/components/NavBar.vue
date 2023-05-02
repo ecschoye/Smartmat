c5c1caace299a8b66d27f57d3b8a2299d4f8eaf5
Kristian Vaula <kristvje@stud.ntnu.no>
2023-05-02 10:57:41 +0200

    Removed unused component data

--- frontend/components/NavBar.vue
+++ frontend/components/NavBar.vue
@@ -12,7 +12,7 @@
       <NuxtLink :to="localePath('/')" class="text-md font-semibold leading-6 text-gray-900 hover:transform hover:-translate-y-0.5 hover:scale-125 hover:text-green-custom">{{t('statistics')}}</NuxtLink>
     </div>
     <div class="flex flex-1 space-x-2 items-center justify-end">
-      <FridgeSelector v-if="loggedIn" @selected-fridge-event="handleFridgeEvent" />
+      <FridgeSelector v-if="loggedIn"/>
       <div class=" w-8"></div>
       <div class="flex lg:hidden">
         <button type="button" class="-m-2.5 inline-flex items-center justify-center rounded-md min-w-fit p-2.5 text-gray-700" @click="mobileMenuOpen = true">
@@ -123,10 +123,6 @@ export default defineComponent({
     }
   },
   methods: {
-    handleFridgeEvent(value: any) {
-      this.selected = value;
-      this.refridgeratorStore.setSelectedRefrigerator(value);
-    },
     async handleLogOut() {
         try{
           const response = await postLogOut();

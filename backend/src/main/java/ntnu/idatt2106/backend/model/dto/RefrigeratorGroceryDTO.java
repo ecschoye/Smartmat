c638bb6902686a1cd925a558871192932df61223
daniesky <daniesky@stud.ntnu.no>
2023-05-02 11:11:52 +0200

    added unit to DTO

--- backend/src/main/java/ntnu/idatt2106/backend/model/dto/RefrigeratorGroceryDTO.java
+++ backend/src/main/java/ntnu/idatt2106/backend/model/dto/RefrigeratorGroceryDTO.java
@@ -3,6 +3,7 @@ package ntnu.idatt2106.backend.model.dto;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import ntnu.idatt2106.backend.model.Unit;
 import ntnu.idatt2106.backend.model.grocery.RefrigeratorGrocery;
 
 import java.util.Date;
@@ -17,10 +18,15 @@ public class RefrigeratorGroceryDTO {
     private long id;
     private Date physicalExpireDate;
     private GroceryDTO grocery;
+    private Unit unit;
+    private int quantity;
+
 
     public RefrigeratorGroceryDTO(RefrigeratorGrocery refrigeratorGrocery) {
         this.id = refrigeratorGrocery.getId();
         this.physicalExpireDate = refrigeratorGrocery.getPhysicalExpireDate();
         this.grocery = new GroceryDTO(refrigeratorGrocery.getGrocery());
+        this.unit = refrigeratorGrocery.getUnit();
+        this.quantity = refrigeratorGrocery.getQuantity();
     }
 }

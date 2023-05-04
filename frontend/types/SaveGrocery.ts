afd0d4be53acdbf56cb26e34e692a699f9015a19
Kristian Vaula <kristvje@stud.ntnu.no>
2023-05-04 15:46:42 +0200

    Unit in savegrocery

--- frontend/types/SaveGrocery.ts
+++ frontend/types/SaveGrocery.ts
@@ -1,5 +1,8 @@
+import type { Unit } from "./UnitType";
+
 export interface SaveGrocery {
     groceryId: Number;
     quantity: Number;
+    unit : Unit,
     foreignKey: Number; //can be used for both shoppingListId and shoppingCartId
 }

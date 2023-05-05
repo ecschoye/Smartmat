984fb0cf11172559e9f8fb22c8ea4de66c5f8baa
Hans Magne Asheim <hmasheim@stud.ntnu.no>
2023-05-05 19:27:39 +0200

    doc: add javadoc for repositories

--- backend/src/main/java/ntnu/idatt2106/backend/repository/ShoppingListRepository.java
+++ backend/src/main/java/ntnu/idatt2106/backend/repository/ShoppingListRepository.java
@@ -9,15 +9,37 @@ import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
+/**
+ * Repository for shoppig list
+ */
 @Repository
 public interface ShoppingListRepository extends JpaRepository<ShoppingList, Long> {
+    /**
+     * Finds a shopping list by it's ID
+     * @param id ID of shopping list
+     * @return Optional of shopping list
+     */
     Optional<ShoppingList> findById(Long id);
 
+    /**
+     * Finds a shopping list by refrigerator id
+     * @param id ID to refrigerator
+     * @return Optional of shopping list
+     */
     @Query("SELECT s FROM ShoppingList s WHERE s.refrigerator.id = :refrigeratorId")
     Optional<ShoppingList> findByRefrigeratorId(@Param(value = "refrigeratorId") Long id);
 
+    /**
+     * Finds the refrigerator assosiated shopping list id
+     * @param id ID to shopping list
+     * @return Refrigerator object for the shopping list
+     */
     @Query(value = "SELECT s.refrigerator FROM ShoppingList s WHERE s.id = :id")
     Refrigerator findRefrigeratorById(Long id);
 
+    /**
+     * Removes a shopping list by refrigerator ID
+     * @param refrigeratorId ID to refrigerator
+     */
     void removeByRefrigerator_Id(long refrigeratorId);
 }

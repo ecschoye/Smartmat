02ed84562aabe7ef6efb2b571c33e61e97321e6d
Hans Magne Asheim <hmasheim@stud.ntnu.no>
2023-05-05 18:27:00 +0200

    add: javadoc for models

--- backend/src/main/java/ntnu/idatt2106/backend/controller/ShoppingListController.java
+++ backend/src/main/java/ntnu/idatt2106/backend/controller/ShoppingListController.java
@@ -26,7 +26,10 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-
+/**
+ * Controller for shopping list
+ * The controller contains method to create a shopping list and operate on groceries in shopping list
+ */
 @RestController
 @RequestMapping("/api/shopping-list")
 @RequiredArgsConstructor
@@ -37,7 +40,13 @@ public class ShoppingListController {
 
     Logger logger = LoggerFactory.getLogger(ShoppingListController.class);
 
-
+    /**
+     *
+     * Creates a shopping list for the specified refrigerator.
+     * @param refrigeratorId the id of the refrigerator for which to create the shopping list
+     * @return a ResponseEntity containing the id of the newly created shopping list and an HTTP status code
+     * @throws RefrigeratorNotFoundException if no refrigerator with the specified id is found
+     */
     @Operation(summary = "Create a shopping list for a refrigerator")
     @ApiResponses(value = {
             @ApiResponse(responseCode = "200", description = "Shopping list created successfully", content = @Content(schema = @Schema(type = "long"))),
@@ -55,6 +64,12 @@ public class ShoppingListController {
         return new ResponseEntity<>(shoppingListId, HttpStatus.OK);
     }
 
+    /**
+     * Retrieves the list of groceries from a shopping list with the specified id.
+     * @param shoppingListId the id of the shopping list for which to retrieve the groceries
+     * @return a ResponseEntity containing the list of groceries and an HTTP status code
+     * @throws NoGroceriesFound if no groceries are found in the specified shopping list
+     */
     @Operation(summary = "Get groceries from a shopping list by shopping list id")
     @ApiResponses(value = {
             @ApiResponse(responseCode = "200", description = "List of groceries fetched successfully", content = @Content(schema = @Schema(implementation = ShoppingListElementDTO.class))),
@@ -71,6 +86,13 @@ public class ShoppingListController {
         return new ResponseEntity<>(groceries, HttpStatus.OK);
     }
 
+    /**
+     * Retrieves the list of groceries with the specified category id from a shopping list with the specified id.
+     * @param shoppingListId the id of the shopping list for which to retrieve the groceries
+     * @param categoryId the id of the category for which to retrieve the groceries
+     * @return a ResponseEntity containing the list of groceries with the specified category id and an HTTP status code
+     * @throws NoGroceriesFound if no groceries with the specified category id are found in the specified shopping list
+     */
     @Operation(summary = "Get groceries from categorized shopping list by shopping list and category id")
     @ApiResponses(value = {
             @ApiResponse(responseCode = "200", description = "Groceries fetched successfully", content = @Content(array = @ArraySchema(schema = @Schema(implementation = ShoppingListElementDTO.class)))),
@@ -89,6 +111,12 @@ public class ShoppingListController {
         return new ResponseEntity<>(groceries, HttpStatus.OK);
     }
 
+    /**
+     * Retrieves a list of suggested groceries from the refrigerator for a given shopping list ID.
+     * @param shoppingListId ID of the shopping list for which suggested groceries are requested
+     * @return a ResponseEntity object containing a list of suggested groceries and an HTTP status code
+     * @throws NoGroceriesFound if no suggested groceries are found for the shopping list
+     */
     @Operation(summary = "Get suggested groceries from refrigerator for a shopping list")
     @ApiResponses(value = {
             @ApiResponse(responseCode = "200", description = "List of suggested groceries from refrigerator for the shopping list", content = @Content(array = @ArraySchema(schema = @Schema(implementation = ShoppingCartElementDTO.class)))),
@@ -106,6 +134,12 @@ public class ShoppingListController {
         return new ResponseEntity<>(groceries, HttpStatus.OK);
     }
 
+    /**
+     * Retrieves a list of categories for a given shopping list ID.
+     * @param shoppingListId ID of the shopping list for which categories are requested
+     * @return a ResponseEntity object containing a list of categories and an HTTP status code
+     * @throws CategoryNotFound if the requested shopping list or category was not found
+     */
     @Operation(summary = "Get categories from a shopping list")
     @ApiResponses(value = {
             @ApiResponse(responseCode = "200", description = "Categories retrieved successfully", content = @Content(array = @ArraySchema(schema = @Schema(implementation = Category.class)))),
@@ -124,6 +158,18 @@ public class ShoppingListController {
         return new ResponseEntity<>(categories, HttpStatus.OK);
     }
 
+
+    /**
+     * Saves a grocery to a shopping list.
+     * @param saveGroceryRequest the request containing the grocery and shopping list IDs
+     * @param request the HTTP servlet request
+     * @return a ResponseEntity with a SuccessResponse object indicating that the grocery was added successfully
+     * @throws SaveException if an error occurs while saving the grocery to the shopping list
+     * @throws UserNotFoundException if the user making the request is not found
+     * @throws ShoppingListNotFound if the shopping list specified in the request is not found
+     * @throws UnauthorizedException if the user making the request is not authorized to access the shopping list
+     * @throws NoSuchElementException if the grocery specified in the request is not found
+     */
     @Operation(summary = "Save a grocery to a shopping list")
     @ApiResponses(value = {
             @ApiResponse(responseCode = "200", description = "The grocery was added successfully", content = @Content(schema = @Schema(implementation = SuccessResponse.class))),
@@ -141,6 +187,17 @@ public class ShoppingListController {
         return new ResponseEntity<>(new SuccessResponse("The grocery was added successfully", HttpStatus.OK.value()), HttpStatus.OK);
     }
 
+    /**
+     * Edits a grocery in a shopping list.
+     * @param groceryShoppingListId the ID of the grocery to edit
+     * @param quantity the new quantity of the grocery
+     * @param httpRequest the HTTP servlet request
+     * @return a ResponseEntity with the edited GroceryShoppingList object
+     * @throws SaveException if an error occurs while saving the edited grocery to the shopping list
+     * @throws NoGroceriesFound if the grocery specified by the ID is not found in the shopping list
+     * @throws UserNotFoundException if the user making the request is not found
+     * @throws UnauthorizedException if the user making the request is not authorized to access the shopping list
+     */
     @Operation(summary = "Edit a grocery in a shopping list")
     @ApiResponses(value = {
             @ApiResponse(responseCode = "200", description = "The grocery was edited successfully", content = @Content(schema = @Schema(implementation = GroceryShoppingList.class))),
@@ -159,6 +216,20 @@ public class ShoppingListController {
         return new ResponseEntity<>(grocery, HttpStatus.OK);
     }
 
+
+    /**
+     * Edit a grocery in the refrigerator shopping list.
+     * @param groceryRefrigeratorShoppingListId the ID of the grocery in the refrigerator shopping list to edit.
+     * @param quantity the new quantity of the grocery item.
+     * @param httpRequest the HTTP request.
+     * @return ResponseEntity containing SuccessResponse with status 200 if the grocery was edited successfully.
+     * @throws SaveException if an error occurs while saving the grocery.
+     * @throws NoGroceriesFound if the grocery is not found in the shopping list.
+     * @throws UserNotFoundException if the user associated with the request is not found.
+     * @throws UnauthorizedException if the user is not authorized to access the resource.
+     * @throws ShoppingListNotFound if the shopping list is not found.
+     * @throws NoSuchElementException if the grocery list ID is not found.
+     */
     @Operation(summary = "Edit a grocery in the refrigerator shopping list")
     @ApiResponses(value = {
             @ApiResponse(responseCode = "200", description = "The grocery was edited successfully", content = @Content(schema = @Schema(implementation = SuccessResponse.class))),
@@ -177,7 +248,15 @@ public class ShoppingListController {
         return new ResponseEntity<>(new SuccessResponse("The grocery was edited successfully", HttpStatus.OK.value()), HttpStatus.OK);
     }
 
-
+    /**
+     * Remove a grocery from a shopping list.
+     * @param groceryListId the ID of the grocery to remove from the shopping list.
+     * @param httpRequest the HTTP request.
+     * @return ResponseEntity containing Boolean with status 200 if the grocery was removed successfully.
+     * @throws UnauthorizedException if the user is not authorized to access the resource.
+     * @throws NoGroceriesFound if the grocery is not found in the shopping list.
+     * @throws UserNotFoundException if the user associated with the request is not found.
+     */
     @Operation(summary = "Remove a grocery from a shopping list")
     @ApiResponses(value = {
             @ApiResponse(responseCode = "200", description = "The grocery was removed successfully", content = @Content(schema = @Schema(implementation = Boolean.class))),
@@ -196,6 +275,15 @@ public class ShoppingListController {
         return new ResponseEntity<>(true, HttpStatus.OK);
     }
 
+    /**
+     * Delete a refrigerator grocery from a shopping list.
+     * @param refrigeratorShoppingListId The id of the refrigerator grocery to be deleted.
+     * @param httpRequest The HttpServletRequest of the request.
+     * @return A ResponseEntity<Boolean> indicating whether the deletion was successful.
+     * @throws UnauthorizedException If the user is not authenticated or authorized to access the resource.
+     * @throws NoGroceriesFound If no groceries are found in the shopping list.
+     * @throws UserNotFoundException If the user is not found.
+     */
     @Operation(summary = "Delete a refrigerator grocery from a shopping list")
     @ApiResponses(value = {
             @ApiResponse(responseCode = "200", description = "The grocery was deleted successfully", content = @Content(schema = @Schema(implementation = SuccessResponse.class))),
@@ -214,6 +302,12 @@ public class ShoppingListController {
         return new ResponseEntity<>(true, HttpStatus.OK);
     }
 
+    /**
+     * Get requested groceries from a shopping list.
+     * @param shoppingListId The id of the shopping list.
+     * @return A ResponseEntity<List<ShoppingListElementDTO>> containing the requested groceries.
+     * @throws NoGroceriesFound If no groceries are found in the shopping list.
+     */
     @Operation(summary = "Get requested groceries from a shopping list")
     @ApiResponses(value = {
             @ApiResponse(responseCode = "200", description = "Requested groceries retrieved successfully", content = @Content(schema = @Schema(implementation = List.class))),
@@ -231,6 +325,14 @@ public class ShoppingListController {
         return new ResponseEntity<>(groceries, HttpStatus.OK);
     }
 
+    /**
+     * Retrieves a list of requested groceries from a categorized shopping list with the specified shopping list ID and category ID.
+     * @param shoppingListId the ID of the categorized shopping list to retrieve the requested groceries from
+     * @param categoryId the ID of the category that the requested groceries belong to
+     * @return a ResponseEntity containing a list of ShoppingListElementDTO objects representing the requested groceries, and an HTTP status code indicating success or failure
+     * @throws NoGroceriesFound if no requested groceries were found for the specified shopping list and category
+     * @throws IllegalArgumentException if the request is invalid
+     */
     @Operation(summary = "Get requested groceries from a categorized shopping list")
     @ApiResponses(value = {
             @ApiResponse(responseCode = "200", description = "List of requested groceries from the categorized shopping list is returned", content = @Content(array = @ArraySchema(schema = @Schema(implementation = ShoppingListElementDTO.class)))),
@@ -250,6 +352,18 @@ public class ShoppingListController {
         return new ResponseEntity<>(groceries, HttpStatus.OK);
     }
 
+    /**
+     * Transfers a grocery item with the specified grocery shopping list ID from a shopping list to the shopping cart.
+     * @param groceryShoppingListId the ID of the grocery item in the shopping list to transfer to the shopping cart
+     * @param httpRequest the HTTP request containing the user's authentication and authorization information
+     * @return a ResponseEntity containing a Boolean value indicating whether the transfer was successful, and an HTTP status code indicating success or failure
+     * @throws UnauthorizedException if the user is not authenticated or authorized to access the resource
+     * @throws NoGroceriesFound if the specified grocery item was not found in the shopping list
+     * @throws UserNotFoundException if the user could not be found
+     * @throws ShoppingCartNotFound if the shopping cart could not be found
+     * @throws SaveException if there was an error saving the changes to the database
+     * @throws NoSuchElementException if the specified grocery item or shopping cart was not found
+     */
     @Operation(summary = "Transfer a grocery item from a shopping list to the shopping cart")
     @ApiResponses(value = {
             @ApiResponse(responseCode = "200", description = "The grocery was transferred successfully", content = @Content(schema = @Schema(implementation = SuccessResponse.class))),
@@ -268,6 +382,17 @@ public class ShoppingListController {
         return new ResponseEntity<>(true, HttpStatus.OK);
     }
 
+    /**
+     * This method transfers a refrigerator grocery item to the shopping cart for the authenticated user.
+     * @param refrigeratorShoppingListId the ID of the grocery item in the refrigerator shopping list to be transferred to the cart.
+     * @param httpRequest the HTTP request object containing the authentication token of the user making the request.   @return ResponseEntity<Boolean> the response entity containing a boolean indicating whether the transfer was successful, and an HTTP status code.
+     * @throws UnauthorizedException if the user is not authenticated or authorized to access the resource.
+     * @throws NoGroceriesFound if the requested grocery item was not found in the refrigerator shopping list.
+     * @throws UserNotFoundException if the user making the request is not found.
+     * @throws ShoppingCartNotFound if the shopping cart for the authenticated user is not found.
+     * @throws SaveException if an error occurs while saving the transfer.
+     * @throws NoSuchElementException if the requested grocery item does not exist.
+     */
     @Operation(summary = "Transfer a refrigerator grocery to the shopping cart")
     @ApiResponses(value = {
             @ApiResponse(responseCode = "200", description = "The grocery was transferred successfully", content = @Content(schema = @Schema(implementation = SuccessResponse.class))),
--- backend/src/main/java/ntnu/idatt2106/backend/model/dto/shoppingCartElement/ShoppingCartElementDTO.java
+++ backend/src/main/java/ntnu/idatt2106/backend/model/dto/shoppingCartElement/ShoppingCartElementDTO.java
@@ -4,6 +4,9 @@ import ntnu.idatt2106.backend.model.dto.UnitDTO;
 import ntnu.idatt2106.backend.model.grocery.GroceryShoppingCart;
 import ntnu.idatt2106.backend.model.grocery.RefrigeratorShoppingList;
 
+/**
+ * DTO for an item in the shopping cart
+ */
 public class ShoppingCartElementDTO {
     private long id;
     private long groceryId;
@@ -12,6 +15,10 @@ public class ShoppingCartElementDTO {
     private UnitDTO unitDTO;
     private String categoryName;
 
+    /**
+     * Constructor to create a shopping cart from a grocery item in the shopping cart
+     * @param element grocery item from shopping cart
+     */
     public ShoppingCartElementDTO(GroceryShoppingCart element) {
         this.id = element.getId();
         this.groceryId = element.getGrocery().getId();
@@ -21,6 +28,10 @@ public class ShoppingCartElementDTO {
         this.categoryName = element.getGrocery().getSubCategory().getCategory().getName();
     }
 
+    /**
+     * Constructor to create a shopping cart from a grocery item in the refrigerator shopping list
+     * @param element grocery item from refrigerator shopping list
+     */
     public ShoppingCartElementDTO(RefrigeratorShoppingList element) {
         this.id = element.getId();
         this.groceryId = element.getGrocery().getId();
@@ -30,26 +41,50 @@ public class ShoppingCartElementDTO {
         this.categoryName = element.getGrocery().getSubCategory().getCategory().getName();
     }
 
+    /**
+     * Getter for id
+     * @return id to the shopping cart element
+     */
     public long getId() {
         return id;
     }
 
+    /**
+     * Getter for the grocery id
+     * @return grocery id
+     */
     public long getGroceryId() {
         return groceryId;
     }
 
+    /**
+     * Getter for description
+     * @return description of grocery
+     */
     public String getDescription() {
         return description;
     }
 
+    /**
+     * Getter for quantity of shopping cart element
+     * @return quantity for shopping cart element
+     */
     public int getQuantity() {
         return quantity;
     }
 
+    /**
+     * Getter for unit DTO
+     * @return unit DTO
+     */
     public UnitDTO getUnitDTO() {
         return unitDTO;
     }
 
+    /**
+     * Getter for the category name
+     * @return category name
+     */
     public String getCategoryName() {
         return categoryName;
     }

77e974db4a6987affcb75a56e4513bda70dbefb1
eriskjel <eriskjel@stud.ntnu.no>
2023-05-05 22:44:15 +0200

    testdata cd config

--- backend/src/main/java/ntnu/idatt2106/backend/config/TestDataSerializer.java
+++ backend/src/main/java/ntnu/idatt2106/backend/config/TestDataSerializer.java
@@ -34,7 +34,7 @@ import java.util.Optional;
  */
 @Component
 @RequiredArgsConstructor
-@Profile("dev")
+@Profile("prod")
 public class TestDataSerializer {
 
     private final CategoryRepository categoryRepository;

8db1a4316c81d610149b06363c0f1f54d06e4480
eriskjel <eriskjel@stud.ntnu.no>
2023-05-05 11:30:27 +0200

    more RecipePool.vue tests

--- frontend/components/WeeklyMenu/RecipeWeeklyCard.spec.ts
+++ frontend/components/WeeklyMenu/RecipeWeeklyCard.spec.ts
@@ -12,8 +12,8 @@ describe('YourComponentName.vue', () => {
         name: 'Test Recipe',
         url: 'https://example.com/test-recipe.jpg',
         ingredients: [
-            { name: 'Ingredient 1', quantity: '1 cup' },
-            { name: 'Ingredient 2', quantity: '2 cups' },
+            {name: 'Ingredient 1', quantity: '1 cup'},
+            {name: 'Ingredient 2', quantity: '2 cups'},
         ],
     };
 
@@ -24,12 +24,86 @@ describe('YourComponentName.vue', () => {
                 NuxtLink: true, // Add this line to stub the nuxt-link component
             }
         },
-        props: { recepeInfo: mockRecipe, lockedBoolean: false },
+        props: {recepeInfo: mockRecipe, lockedBoolean: false},
     });
 
     it('renders recipe name', () => {
         expect(wrapper.find('.recepe-title').text()).toBe(mockRecipe.name);
     });
 
-    // Add more tests as needed
+    it('renders locked icon when lockedBoolean is true', () => {
+        const wrapper = mount(RecipeWeeklyCard, {
+            global: {
+                mocks: globalMocks,
+                stubs: {
+                    NuxtLink: true,
+                },
+            },
+            props: {recepeInfo: mockRecipe, lockedBoolean: true},
+        });
+
+        expect(wrapper.find('.lock-icon').attributes('src')).toContain('Locked.webp');
+    });
+
+    it('renders unlocked icon when lockedBoolean is false', () => {
+        const wrapper = mount(RecipeWeeklyCard, {
+            global: {
+                mocks: globalMocks,
+                stubs: {
+                    NuxtLink: true,
+                },
+            },
+            props: {recepeInfo: mockRecipe, lockedBoolean: false},
+        });
+
+        expect(wrapper.find('.lock-icon').attributes('src')).toContain('unlock.png');
+    });
+
+    it('renders ingredients when showIngredients is true', async () => {
+        const wrapper = mount(RecipeWeeklyCard, {
+            global: {
+                mocks: globalMocks,
+                stubs: {
+                    NuxtLink: true,
+                },
+            },
+            props: {recepeInfo: mockRecipe, lockedBoolean: false},
+        });
+
+        await wrapper.find('button:first-child').trigger('click');
+
+        expect(wrapper.find('.ingredients').exists()).toBe(true);
+    });
+
+    it('emits seeRecipeEvent when "See Recipe" button is clicked', async () => {
+        const wrapper = mount(RecipeWeeklyCard, {
+            global: {
+                mocks: globalMocks,
+                stubs: {
+                    NuxtLink: true,
+                },
+            },
+            props: {recepeInfo: mockRecipe, lockedBoolean: false},
+        });
+
+        await wrapper.find('button:nth-child(2)').trigger('click');
+
+        expect(wrapper.emitted().seeRecipeEvent).toBeTruthy();
+    });
+
+    it('emits removeEvent when "Remove" button is clicked', async () => {
+        const wrapper = mount(RecipeWeeklyCard, {
+            global: {
+                mocks: globalMocks,
+                stubs: {
+                    NuxtLink: true,
+                },
+            },
+            props: {recepeInfo: mockRecipe, lockedBoolean: false},
+        });
+
+        await wrapper.find('button:nth-child(3)').trigger('click');
+
+        expect(wrapper.emitted().removeEvent).toBeTruthy();
+    });
 });
